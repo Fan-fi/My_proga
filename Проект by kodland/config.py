37a326d22cf0cbb1271965bdab55a7e94a296604
Fan-fi <naboychenko1201@gmail.com>
2026-05-01 17:28:52 +0300

    Update config.py

--- Проект by kodland/config.py
+++ Проект by kodland/config.py
@@ -1,5 +1,5 @@
 
-API_TOKEN = "8397741728:AAHkiT4YZxFKMbydL7P02WMVNiRLfM5tsys"
+API_TOKEN = ""
 MUTE_MINUTES = 10          # время мута по умолчанию (минут)
 WARN_LIMIT = 5             # предупреждений до автоматического мута
 DB_NAME = "moderator.db"
