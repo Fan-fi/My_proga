97dccfeb7959c6953b35fcb434b202a8e8bc7d22
Fan-fi <naboychenko1201@gmail.com>
2026-05-07 18:29:16 +0300

    Add files via upload

---
+++ Проект by kodland/config.py
@@ -0,0 +1,6 @@
+
+API_TOKEN = "8397741728:AAHkiT4YZxFKMbydL7P02WMVNiRLfM5tsys"
+MUTE_MINUTES = 10          # время мута по умолчанию (минут)
+WARN_LIMIT = 5             # предупреждений до автоматического мута
+DB_NAME = "moderator.db"
+OWNER_ID = 1930961190       # ваш Telegram ID (число)
