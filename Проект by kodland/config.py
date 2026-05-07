84f78d800e1c45c0b5585c20bc92c54ae2514ff0
Fan-fi <naboychenko1201@gmail.com>
2026-05-07 18:28:15 +0300

    Delete Проект by kodland directory

--- Проект by kodland/config.py
+++
@@ -1,6 +0,0 @@
-
-API_TOKEN = ""
-MUTE_MINUTES = 10          # время мута по умолчанию (минут)
-WARN_LIMIT = 5             # предупреждений до автоматического мута
-DB_NAME = "moderator.db"
-OWNER_ID = 1930961190       # ваш Telegram ID (число)
